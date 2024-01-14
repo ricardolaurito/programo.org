90af7feffb714f15b6ad14864fed0c721fda2d1f
Ricardo Laurito <75488296+ricardolaurito@users.noreply.github.com>
2024-01-14 05:45:57 +0000

    Adicionado os arquivos database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response
     .status(200)
     .json("programo.org um refúgio para programadores, UI e UX Designers");
